d5dcb313f66af8f81f2e61f9eaf473696fc88a28
medali2323 <med.ali.bouacida@gmail.com>
2024-01-06 19:44:08 +0100

    17 12

--- app/Http/Controllers/api/FactureController.php
+++ app/Http/Controllers/api/FactureController.php
@@ -9,88 +9,25 @@ use Illuminate\Support\Facades\Validator;
 
 class FactureController extends Controller
 {
-    public function index()  {
-        $factures= facture::all();
-        if($factures->count()>0)
-         return response()->json([
-             'status'=>200,
-             'factures'=>$factures
-            ],200);
-        
-        else 
-         return response()->json([
-             'status'=>404,
-             'factures'=>' aucun factures'
-            ],404);
-     
-     
-       
-     }
-     public function ajouter(Request $request){
-      $validator= Validator::make($request->all(),[
-         'code'=>'required|string|max:50',
-         'montant_total' => 'required|numeric',
-         'date_facturation' => 'required|date',
-         'tva_id'=>'required',
-         'etat_paiment' => 'required'
+  
+    public function index()
+    {
+        $factures = facture::orderByDesc('date_facturation')->get();
+    
+        if ($factures->count() > 0) {
+            return response()->json([
+                'status' => 200,
+                'factures' => $factures
+            ], 200);
+        } else {
+            return response()->json([
+                'status' => 404,
+                'factures' => 'aucun factures'
+            ], 404);
+        }
+    }
+    
 
- 
-      ]); 
-         if ($validator->fails()) {
-             return response()->json([
-                 'status'=>422,
-                 'ERRORRS'=>$validator->messages() 
-                ],422);
-         }else {
-             $code = $request->input('code');
-             $montant_total = $request->input('montant_total');
-             $date_facturation = $request->input('date_facturation');
-             $tva_id = $request->input('tva_id');
-             $etat_paiment = $request->input('etat_paiment');
-
-             // Create a new instance of the facture model
-             $facture = new facture();
-     
-             // Set the values of the model attributes
-             $facture->code = $code;
-             $facture->montant_total = $montant_total;
-             $facture->date_facturation = $date_facturation;
-             $facture->tva_id = $tva_id;
-             $facture->etat_paiment = $etat_paiment;
-
- 
-             $facture->updated_at = now();
-             $facture->created_at = now();
-     
-     
-             $facture->save();
-             if($facture){
-                 return response()->json([
-                     'status'=>200,
-                     'message'=>"facture created secsusflly",
-                     'facture' => $facture
-
-                    ],200);
-             }else{
-                 return response()->json([
-                     'status'=>500,
-                     'message'=>"un problem quelque part"
-                    ],500);
-             }
-         }
-     }
-     public function getById($id){
-      $facture = facture::find($id);
- 
-      if (!$facture) {
-         return response()->json(
-             [ 'status'=>404,
-             'message' => 'facture non trouvé'
-         ], 404);
-  }
- 
- return response()->json($facture, 200);
- }
  public function update(Request $request, $id)
 {
     // Valider les données du formulaire de mise à jour
